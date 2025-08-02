84a529755bcd53ecff9052eb8023df2580482bb7
ZapoVerde <strong.jeremy@gmail.com>
2025-08-02 14:23:42 +0000

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 // src/App.tsx
+
 import React from 'react';
-import LoginScreen from './ui/screens/LoginScreen';
+import LoginScreen from './ui/screens/LoginScreen'; // <-- UNCOMMENT THIS LINE
 
 function App() {
   return (
     <div className="App">
-      <LoginScreen />
+      {/* You'll put your main application routing here later */}
+      <h1>StoryForge Web App</h1>
+      <p>Welcome to your new StoryForge Web MVP!</p>
+      <LoginScreen /> {/* <-- UNCOMMENT THIS LINE */}
     </div>
   );
 }
